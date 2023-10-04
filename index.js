fcbf75ac03e245ff23814e840f7519fc94e23862
Tuan Anh Nguyen <ntak.ictu@gmail.com>
2023-10-05 00:14:52 +0700

    add code note detail

--- index.js
+++ index.js
@@ -36,16 +36,35 @@ app.get('/', (req, res) => {
 // Route xử lý form tạo note mới
 app.post('/post', (req, res) => {
     const { title, content } = req.body;
-    // Thêm note mới vào cơ sở dữ liệu
-    // Redirect về trang danh sách note
+    const sql = 'INSERT INTO notes (title, content) VALUES (?, ?)';
+    db.query(sql, [title, content], (err, result) => {
+        if (err) {
+            console.error('Error creating a new note:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        // Sau khi thêm note mới thành công, chuyển hướng về trang danh sách note
+        res.redirect('/');
+    });
 });
+
 
 // Route hiển thị chi tiết note
 app.get('/post/:id', (req, res) => {
     const noteId = req.params.id;
-    // Truy vấn cơ sở dữ liệu để lấy chi tiết note
-    // Hiển thị chi tiết note
+    const sql = 'SELECT * FROM notes WHERE id = ?';
+    db.query(sql, [noteId], (err, result) => {
+        if (err) {
+            console.error('Error querying the database:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        if (result.length === 0) {
+            return res.status(404).send('Note not found');
+        }
+        // Truy vấn thành công, hiển thị chi tiết note
+        res.render('noteDetail', { note: result[0] }); // Điều này giả định bạn có một tệp view 'noteDetail.ejs'
+    });
 });
+
 
 // Route sửa note
 app.get('/edit/:id', (req, res) => {
